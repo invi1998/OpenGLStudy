f44b71eb0252c594b0afb81452122fdd5c8d3981
invi1998 <2267661826@qq.com>
2022-10-01 18:36:12 +0800

    绘制圆，通过提前准备好顶点数据，进行性能优化

--- OpenGL/OpenGL.cpp
+++ OpenGL/OpenGL.cpp
@@ -9,6 +9,13 @@
 class SamplerTriangle : public OpenGLWindow
 {
 public:
+	struct float3
+	{
+		float x, y, z;
+	};
+
+	float3 _circle[360 * 3];
+
 	void render() override
 	{
 		// 指定矩阵模式，这里传递 GL_PROJECTION 是告诉OpenGL我现在想操作的是 投影
@@ -24,22 +31,26 @@ public:
 		float cz = 0;
 		float cr = 200;
 
-		glBegin(GL_TRIANGLES);
-
 		for (int i = 0; i < 360; ++i)
 		{
-			float x = (float)cos((double)i * M_PI / 180) * cr + cx;
-			float y = (float)sin((double)i * M_PI / 180) * cr + cy;
+			_circle[i * 3 + 0].x = cx;
+			_circle[i * 3 + 0].y = cy;
+			_circle[i * 3 + 0].z = cz;
 
-			float x1 = (float)cos((double)(i + 1) * M_PI / 180) * cr + cx;
-			float y1 = (float)sin((double)(i + 1) * M_PI / 180) * cr + cy;
+			_circle[i * 3 + 1].x = (float)cos((double)i * M_PI / 180) * cr + cx;
+			_circle[i * 3 + 1].y = (float)sin((double)i * M_PI / 180) * cr + cy;
+			_circle[i * 3 + 1].z = cz;
 
-			glVertex3f(cx, cy, cz);
-			glVertex3f(x, y, cz);
-			glVertex3f(x1, y1, cz);
+			_circle[i * 3 + 2].x = (float)cos((double)(i + 1) * M_PI / 180) * cr + cx;
+			_circle[i * 3 + 2].y = (float)sin((double)(i + 1) * M_PI / 180) * cr + cy;
+			_circle[i * 3 + 2].z = cz;
 		}
-
-		glEnd();
+		// 启用顶点数组
+		glEnableClientState(GL_VERTEX_ARRAY);
+		// 绑定顶点数据 大小、类型、步幅 数据指针
+		glVertexPointer(3, GL_FLOAT, sizeof(float3), _circle);
+		// 调用 drawArray
+		glDrawArrays(GL_TRIANGLES, 0, 3 * 360);
 	}
 };
 
