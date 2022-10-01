a65e7cba9f86b8f18a72887692e3ccc8b26645fa
invi1998 <2267661826@qq.com>
2022-10-01 09:59:14 +0800

    OpenGL正交投影 glOrtho()

--- OpenGL/OpenGL.cpp
+++ OpenGL/OpenGL.cpp
@@ -40,6 +40,9 @@ int __stdcall WinMain(
 	wnd.lpszClassName = _T("lesson01");
 	wnd.style = CS_DBLCLKS | CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
 
+	int width = 1000;
+	int height = 1000;
+
 	RegisterClassEx(&wnd);
 
 	//2 创建窗口
@@ -50,8 +53,8 @@ int __stdcall WinMain(
 		WS_OVERLAPPEDWINDOW,
 		100,
 		100,
-		480,
-		320,
+		width,
+		height,
 		0,
 		0,
 		hInstance,
@@ -76,6 +79,12 @@ int __stdcall WinMain(
 		return 0;
 	}
 
+	// 利用windows API 得到正确的窗口宽高（减去标题栏）
+	RECT rect;
+	GetClientRect(hWnd, &rect);
+	width = rect.right - rect.left;
+	height = rect.bottom - rect.top;
+
 	MSG     msg = { 0 };
 
 	//4 消息循环
@@ -91,8 +100,26 @@ int __stdcall WinMain(
 			/**
 			*   调用绘制函数进行绘制
 			*/
-			glClearColor(1, 1, 1, 1);
+			glClearColor(0.4, 0.1, 0.6, 1);
 			glClear(GL_COLOR_BUFFER_BIT);
+			// 指定视口
+			glViewport(0, 0, width, height);
+			// 指定矩阵模式，这里传递 GL_PROJECTION 是告诉OpenGL我现在想操作的是 投影
+			// 指定以下操作针对投影矩阵
+			glMatrixMode(GL_PROJECTION);
+			// 将投影矩阵清空为单位矩阵
+			glLoadIdentity();
+			// 指定正交投影空间,产生投影矩阵
+			glOrtho(0, width, height, 0, -100, 100);
+
+			// 告诉OpenGL我下面想画一个线
+			glBegin(GL_LINES);
+			// 指定线的起始点和终点
+			glVertex3f(0, 0, 0);
+			glVertex3f(500, 500, 0);
+
+			glEnd();
+
 			context.swapBuffer();
 		}
 	}
